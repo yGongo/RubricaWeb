af7045d38202a7086a422c91f8387b3cc2153d71
Eugenio Malenchi <114256839+yGongo@users.noreply.github.com>
2024-03-13 10:47:22 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using rubricaWeb.Models;
+
+namespace rubricaWeb.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        List<string> s = new List<string>();
+
+        using( var sr = new StreamReader("Data/Contatti.csv"))
+        {
+            while(!sr.EndOfStream)
+                s.Add(sr.ReadLine());
+        }
+        return View(s);
+    }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
